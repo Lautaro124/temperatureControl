fb5b3c3ea99efa9fc85c7843d8b8e93c1a0fcd71
Lautaro124 <lautaro.gonzalez4949@gmail.com>
2023-10-06 13:58:17 -0300

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  useColorScheme,
+} from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+import BluetoohScreen from './screens/bluetoohConect/bluetooh.screen';
+
+function App(): JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <BluetoohScreen />
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/screens/bluetoohConect/bluetooh.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const BluetoohScreen = () => {
+  return (
+    <View>
+      <Text>Conexion con bluetooh</Text>
+    </View>
+  );
+};
+
+export default BluetoohScreen;
